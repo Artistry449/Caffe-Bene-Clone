c8a538116e9ea3f05608d1eaf3668ca98910581b
Artistry449 <artistry449@gmail.com>
2024-04-01 13:15:04 +0800

    added pages

--- script.js
+++ script.js
@@ -4,9 +4,12 @@ const navBtns = document.querySelectorAll(".nav-link");
 navBtns.forEach((nodeEl) => {
     nodeEl.addEventListener("mouseover", (event) => {
         document.getElementById("external-nav").classList.remove("hidden");
-    })
+    });
 
-    nodeEl.addEventListener("mouseout", (event) => {
-        document.getElementById("external-nav").classList.add("hidden");
-    })
 })
+
+const external_nav = document.getElementById("external-nav");
+
+external_nav.addEventListener("mouseleave", (event) => {
+    document.getElementById("external-nav").classList.add("hidden");
+});
